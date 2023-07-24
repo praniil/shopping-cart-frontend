401f2ac7b959a884b12966ad388e715f95db1db0
praniil <pparajuli15@gmail.com>
2023-07-24 19:27:46 +0545

    feat: added querySelectors

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,6 +2,7 @@ import { FunctionComponent, useEffect } from "react";
 import useLocalStorageState from "use-local-storage-state";
 import classes from './header.module.css'
 import { CartWidget } from "../CartWidget/CartWidget";
+import { CartProps } from "../Products/Products"
 
 import {
     BrowserRouter,
@@ -16,12 +17,32 @@ export const Header: FunctionComponent = () => {
             window.removeEventListener("scroll", () => shrinkHeader())
         }
     }, []) //empty array := only once executed
-    const shrinkHeader = ()=>{
+    const shrinkHeader = () => {
         const distance_from_top = 140
-        const headerElement = document.querySelector("header")
+        const headerElement = document.querySelector("header") as HTMLElement
+        const logoElement = document.querySelectorAll("img")[0] as HTMLElement
+        const cartWidgetElement = document.querySelectorAll("img")[1] as HTMLElement
+        const productsCountElement = document.querySelector("span") as HTMLElement
+        const scrollY = document.body.scrollTop || document.documentElement.scrollTop
+        if (scrollY > distance_from_top) {
+            headerElement.style.transition = "height 200ms ease-in"
+            headerElement.style.height = "80px"
+            logoElement.style.transition = "height 200ms ease-in"
+            logoElement.style.height = "4rem"
+            cartWidgetElement.style.transition = "height 200ms ease-in"
+            cartWidgetElement.style.height = "2rem"
+            productsCountElement.style.transition = "height 200ms ease-in"
+            productsCountElement.style.fontSize = "80px"
+        } else {
+            headerElement.style.height = "150px"
+            logoElement.style.height = "6rem"
+            cartWidgetElement.style.height = "3rem"
+            productsCountElement.style.fontSize = "2em"
+        }
     }
+    const [cart, ] = useLocalStorageState<CartProps>('cart', {})
 
-    var productsCount = 10
+    var productsCount : number = Object.keys(cart || {}).length
     return (
         <header className={classes.header}>
             <div>
