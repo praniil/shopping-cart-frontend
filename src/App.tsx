f9891077609b7bcfa2128efc4d3dd3d78e28a253
praniil <pparajuli15@gmail.com>
2023-07-25 19:42:53 +0545

    feat: added products

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,20 @@ import './App.css';
 import { CartWidget } from './components/CartWidget/CartWidget';
 import { Header } from './components/Header/Header';
 import { HashRouter as Router, Route, Routes } from 'react-router-dom';
+import { Products } from './components/Products/Products';
 
 function App() {
   const productsCount = 10
   return (
-    <Router>
-      <Routes>
+    // <Router>
+    //   <Routes>
         
-    <Route path = "cartwidget" element = {<CartWidget productsCount={productsCount}/>}/>
-    <Route path = "/" element = {<Header/>}/>
-      </Routes>
-    </Router>
+    // <Route path = "cartwidget" element = {<CartWidget productsCount={productsCount}/>}/>
+    // <Route path = "/" element = {<Header/>}/>
+    // <Route path = "products" element = {<Products/>}/>
+    // </Routes>
+    // </Router>
+    <Products/>
     
   );
 }
--- src/components/Products/Products.tsx
+++ src/components/Products/Products.tsx
@@ -1,17 +1,78 @@
-import { FunctionComponent } from "react";
+import { FunctionComponent, useEffect, useState } from "react";
 import classes from "./products.module.css"
+import useLocalStorageState from "use-local-storage-state";
 
-const API_URL = 'http://localhost:8080/api/newproduct'
+const API_URL = 'http://localhost:8080/api/getallproduct'
 
-export type Product ={
-    id : number
-    title : string
-    price : number
-    thumbnail : string
-    image : string
-    quantity : number
+export type Product = {
+    id: number
+    title: string
+    price: number
+    thumbnail: string
+    image: string
+    quantity: number
+}
+//interface is a way to define a contract or structure for an object
+export interface CartProps {
+    [productId: string]: Product       //key of type string
 }
 
-export interface CartProps {
-    [productId: string] : Product
+export const Products: FunctionComponent = () => {
+    const [products, setProducts] = useState<Product[]>([])
+    const [error, setError] = useState(false)
+    const [cart, setCart] = useLocalStorageState<CartProps>('cart', {})
+
+
+    useEffect(() => {
+        fetchData(API_URL)
+    }, [])
+
+    async function fetchData(url: string) {
+        try {
+            const response = await fetch(url)
+            if (response.ok) {
+                const data = await response.json()
+                setProducts(data.products)
+
+            } else {
+                setError(true)
+
+            }
+        } catch (error) {
+            setError(true)
+
+        }
+    }
+
+    const addToCart = (product: Product): void => {
+        product.quantity = 1
+
+        setCart((prevCart) => ({
+            ...prevCart,
+            [product.id]: product,
+        }))
+    }
+
+    const isInCart = (productId: number): boolean => Object.keys(cart || {}).includes(productId.toString())
+    if (error) {
+        return <h3 className={classes.error}> An error occured when fetching data. Please check the API and try again.</h3>
+    }
+
+    return (
+        
+        <section className={classes.productPage}>
+            
+            <h1>Products</h1>
+            <div className={classes.container}>
+                {products.map(product => (
+                    <div className={classes.product} key={product.id} >
+                        <img src={product.thumbnail} alt={product.title} />
+                        <h3>{product.title}</h3>
+                        <p> Price :  {product.price}</p>
+                        <button disabled={isInCart(product.id)} onClick={() => addToCart(product)}> Add to Cart </button>
+                    </div>
+                ))}
+            </div>
+        </section>
+    )
 }
