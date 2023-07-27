0560f3f4997a362a71fbe00bad6751ef4eec6a7b
praniil <pparajuli15@gmail.com>
2023-07-27 10:31:59 +0545

    feat: updated cart

--- src/components/Cart/Cart.tsx
+++ src/components/Cart/Cart.tsx
@@ -1,11 +1,67 @@
-import React from 'react'
+import { FunctionComponent, useEffect } from 'react'
+import useLocalStorageState from 'use-local-storage-state'
 
-const Cart = () => {
+
+import Quantifier from '../Quantifier/Quantifier'
+import { CartProps } from '../Products/Products'
+import TotalPrice from '../TotalPrice/TotalPrice'
+import { Operation } from '../Quantifier/Operation'
+import classes from './cart.module.css'
+import { useLocation } from 'react-router-dom'
+
+
+export const Cart: FunctionComponent = () => {
+  const [cart, setCart] = useLocalStorageState<CartProps>('cart', {})
+  const location = useLocation()
+
+  useEffect(() => {
+    window.scrollTo(0, 0)
+  }, [location])
+
+  const handleRemoveProduct = (productId: number): void => {
+    setCart((prevCart) => {
+      const updatedCart = { ...prevCart }
+      delete updatedCart[productId]
+      return updatedCart
+    })
+  }
+
+  const handleUpdateQuantity = (productId: number, operation: Operation) => {
+    setCart((prevCart) => {
+      const updatedCart = { ...prevCart }
+      if (updatedCart[productId]) {
+        if (operation === 'increase') {
+          updatedCart[productId] = { ...updatedCart[productId], quantity: updatedCart[productId].quantity + 1 }
+        } else {
+          updatedCart[productId] = { ...updatedCart[productId], quantity: updatedCart[productId].quantity - 1 }
+        }
+      }
+      return updatedCart
+    })
+  }
+
+
+  const getProducts = () => Object.values(cart || {})
+
+  const totalPrice = getProducts().reduce((accumulator, product) => accumulator + (product.price * product.quantity), 0)
+
   return (
-    <div>
-      
-    </div>
+    <section className={classes.cart}>
+      <h1>Cart</h1>
+
+      <div className={classes.container}>
+        {getProducts().map(product => (
+          <div className={classes.product} key={product.id}>
+            <img src={product.thumbnail} alt={product.title} />
+            <h3>{product.title}</h3>
+            <Quantifier
+              removeProductCallback={() => handleRemoveProduct(product.id)}
+              productId={product.id}
+              handleUpdateQuantity={handleUpdateQuantity} />
+          </div>
+        ))}
+      </div>
+      <TotalPrice amount={totalPrice} />
+    </section>
   )
 }
-
-export default Cart
